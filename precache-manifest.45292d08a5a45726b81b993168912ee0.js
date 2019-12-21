721c4ccaffe9cf0dc04c74fb1ea91e8db71b8592
David Carlyn <dcarlyn@kent.edu>
2019-12-20 22:18:07 -0500

    Updates

---
+++ precache-manifest.45292d08a5a45726b81b993168912ee0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345c6e773223d66f255ca8387a8b8334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ba5e6e0eac6f7059804",
+    "url": "/static/css/main.fa1233d3.chunk.css"
+  },
+  {
+    "revision": "7218667a95637673d703",
+    "url": "/static/js/2.d80b810e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.d80b810e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ba5e6e0eac6f7059804",
+    "url": "/static/js/main.758872a8.chunk.js"
+  },
+  {
+    "revision": "6f4bf109a2c9ba4263eb",
+    "url": "/static/js/runtime-main.2aea4f80.js"
+  },
+  {
+    "revision": "04db9480775c4f01ee9773d41d5b76ff",
+    "url": "/static/media/DavidCarlynResumeCondensed.04db9480.pdf"
+  },
+  {
+    "revision": "c53ca9bc9044aea30f658dd98267af4b",
+    "url": "/static/media/astrocyteTechnicalPaper.c53ca9bc.pdf"
+  },
+  {
+    "revision": "b0fbfe2db4a2b2567fc9369647898750",
+    "url": "/static/media/profile_picture.b0fbfe2d.jpg"
+  },
+  {
+    "revision": "5a1a68b7ba4fd8252849ab897e6fd4dc",
+    "url": "/static/media/slackcodechangenotification.5a1a68b7.pdf"
+  }
+]);
